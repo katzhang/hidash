187fe89fc99a283c749dcdbbb1da4f7fe2bece01
katzhang-ias <arcadychang@gmail.com>
2015-04-22 00:30:01 -0400

    added after.js

---
+++ after/after.js
@@ -0,0 +1,23 @@
+var after = function(n, func) {
+	var counter = 0;
+	return function() {
+		if (counter >= n) {
+			func();
+			counter = 0;
+		} else {
+			counter++;
+		}
+	}
+};
+
+var sayHi = function() {
+	console.log('hi');
+};
+
+var afterred = after(2, sayHi);
+
+afterred();
+afterred();
+afterred();
+afterred();
+afterred();
